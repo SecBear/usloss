ef8d7e9683e30ec456d610aa01b21c4667aa230c
bryce <thorpebryce@gmail.com>
2024-04-13 20:19:08 -0700

    delete unused mbox

--- phase3/phase3.c
+++ phase3/phase3.c
@@ -33,7 +33,7 @@ void syscall_getcputime(sysargs *args);
    Global Variables
    ----------------------------------------------------------------------- */
 process ProcTable[MAXPROC];     // Array of processes
-semaphore SemTable[MAXSEMS];       // Array of seamphores
+semaphore SemTable[MAXSEMS];    // Array of seamphores
 
 int numSems = 0;                // Global count of semaphores
 int next_sem_id = 0;            // Integer to hold the next semaphore ID
@@ -97,7 +97,6 @@ start2(char *arg)
     {
         // Initialize basic values
         SemTable[i].value = NULL;
-        SemTable[i].mbox = NULL;
         SemTable[i].sid = NULL;
         SemTable[i].status = SEM_UNUSED;  // indicates a semaphore is freshly allocated
 
@@ -108,34 +107,6 @@ start2(char *arg)
         SemTable[i].waiting->count = 0;     
     }
 
-    /*
-     * Create first user-level process and wait for it to finish.
-     * These are lower-case because they are not system calls;
-     * system calls cannot be invoked from kernel mode.
-     * Assumes kernel-mode versions of the system calls
-     * with lower-case names.  I.e., Spawn is the user-mode function
-     * called by the test cases; spawn is the kernel-mode function that
-     * is called by the syscall_handler; spawn_real is the function that
-     * contains the implementation and is called by spawn.
-     *
-     * Spawn() is in libuser.c.  It invokes usyscall()
-     * The system call handler calls a function named spawn() -- note lower
-     * case -- that extracts the arguments from the sysargs pointer, and
-     * checks them for possible errors.  This function then calls spawn_real().
-     *
-     * Here, we only call spawn_real(), since we are already in kernel mode.
-     *
-     * spawn_real() will create the process by using a call to fork1 to
-     * create a process executing the code in spawn_launch().  spawn_real()
-     * and spawn_launch() then coordinate the completion of the phase 3
-     * process table entries needed for the new process.  spawn_real() will
-     * return to the original caller of Spawn, while spawn_launch() will
-     * begin executing the function passed to Spawn. spawn_launch() will
-     * need to switch to user-mode before allowing user code to execute.
-     * spawn_real() will return to spawn(), which will put the return
-     * values back into the sysargs pointer, switch to user-mode, and 
-     * return to the user code that called Spawn.
-     */
     pid = spawn_real("start3", start3, NULL, 4*USLOSS_MIN_STACK, 3);
     pid = wait_real(&status);
 
@@ -406,7 +377,6 @@ int semcreate_real(int init_value)
     // Initialize semaphore values
     SemTable[semID].status = SEM_USED;
     SemTable[semID].value = init_value;
-    SemTable[semID].mbox = MboxCreate(0,0);     // Create semaphore's private mailbox
     SemTable[semID].mutex = MboxCreate(1,0);    // Create semaphore's mutex
     
     numSems++;  // Increment max number of sems
